b1c496e432ae2be39f9f4b90ff828cfa6bfd0caa
Jonathan Martinez <jonathan@jonathans-mbp.attlocal.net>
2015-06-06 20:24:50 -0700

    added comment for buddon

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // this code will evaluate whence you hit the sendmail buddon
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
